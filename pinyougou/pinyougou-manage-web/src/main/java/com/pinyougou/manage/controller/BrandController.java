512f985e43dac6e16ff1f39007e39ab8dea35ea7
wanghua <344409823@qq.com>
2018-12-11 11:09:18 +0800

    分页显示品牌数据

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -3,6 +3,7 @@ package com.pinyougou.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.vo.PageResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,7 +30,15 @@ public class BrandController {
      */
     @GetMapping("/testPage")
     public List<TbBrand> testPage(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer rows) {
-       // return brandService.testPage(page, rows);
-        return (List<TbBrand>) brandService.findPage(page,rows).getRows();
+        // return brandService.testPage(page, rows);
+        return (List<TbBrand>) brandService.findPage(page, rows).getRows();
+    }
+
+    /**
+     * ../brand/findPage.do?page=" + page + "&rows=" + rows
+     */
+    @GetMapping("/findPage")
+    public PageResult findPage(@RequestParam(value = "page", defaultValue = "1") Integer page, @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
+        return brandService.findPage(page, rows);
     }
 }
